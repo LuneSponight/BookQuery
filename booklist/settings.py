eae644014f81eaa257b0e6d64ad7b979b768402d
LuneSponight <weichiawei0@gmail.com>
2023-06-25 14:27:19 +0800

    change names

--- booklist/settings.py
+++ booklist/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = "django-insecure-w=935t08)pk7xsl^64@#*hrbmrl_)(=+5a)4emh$-h20pzz2t(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '192.168.10.3']
-
+ALLOWED_HOSTS = ['localhost', '192.168.10.3', '127.0.0.1']
 
 # Application definition
 
@@ -70,22 +68,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "booklist.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
-	'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bookQuery',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookquery',
         'USER': 'root',
         'PASSWORD': 'mysQlSSnig449*',
         'HOST': '192.168.10.1',
         'PORT': '3306'
-
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -116,7 +110,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
