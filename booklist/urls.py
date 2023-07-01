dc6532e52ad2c29efb5c5bb8ec69aa8c9778739f
LuneSponight <weichiawei0@gmail.com>
2023-07-01 11:26:59 +0800

    querybook .

--- booklist/urls.py
+++ booklist/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..querybook import views
+from querybook import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
