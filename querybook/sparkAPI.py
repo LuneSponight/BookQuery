96e52aa19f981cda6e3abf66f2b80cbf1b3860e6
LuneSponight <weichiawei0@gmail.com>
2023-07-01 11:03:20 +0800

    add querybook sys

--- querybook/sparkAPI.py
+++ querybook/sparkAPI.py
@@ -1,5 +1,7 @@
+import sys
+sys.path.append('/root/django/booklist/querybook')
 
-from . import help
+from . import helpa
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("SparkJDBC").master("spark://master:7077").getOrCreate()
